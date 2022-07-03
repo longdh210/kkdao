f71c8ef044cf0d9dfa8f1e1b0feff4449d09e757
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-07-03 10:26:12 +0700

    Revert "Merge pull request #3 from longdh210/thinhlh"
    This reverts commit 747baa652298a25ac89ee6902760c38b3a2f0d51, reversing
    changes made to 5978889d3520f576707f1e2c82a8a3f13cd423ba.

--- src/components/animatedRoutes.js
+++ src/components/animatedRoutes.js
@@ -6,8 +6,6 @@ import { Routes, Route, useLocation } from "react-router-dom";
 
 import { AnimatePresence } from "framer-motion";
 import About from "../pages/About/About";
-import SignUp from "../pages/SignUp/SignUp";
-import Investor from "../pages/Investor/Investor";
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -22,9 +20,7 @@ function AnimatedRoutes() {
           element={<IntroVideoLandingPage />}
         />
         <Route path="zoomaboutpage" element={<ZoomAboutPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/investor" element={<Investor />} />
-        <Route path="focus" element={<FocusPage />} />
+        <Route path="focus" element={<FocusPage/>}/>
       </Routes>
     </AnimatePresence>
   );
