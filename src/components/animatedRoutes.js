1c200e25d1ba31595e769bb47988916bc2a6287b
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-07-03 12:03:23 +0700

    Squashed commit of the following:
    commit 9a8210e02c4a9661a1be7fe2fc35c673b681d481
    Author: Hoang Thinh <jonhnathankevin@yahoo.com.vn>
    Date:   Sun Jul 3 11:53:46 2022 +0700
    
        [FEAT] update investor UI
    
    commit d124b11f4c346c9f66311bd56757708b8f230015
    Author: Hoang Thinh <jonhnathankevin@yahoo.com.vn>
    Date:   Sun Jul 3 11:21:33 2022 +0700
    
        [FEAT] update Ids
    
    commit 31c63b14a70e39893420da97534383de6d0a5aa9
    Merge: 38fec61 954a22e
    Author: Hoang Thinh <jonhnathankevin@yahoo.com.vn>
    Date:   Sun Jul 3 10:57:34 2022 +0700
    
        Merge branch 'Long' into thinhlh-backup
    
    commit 38fec61f34eae2ec4e95860e5035ab3ece99d220
    Author: Hoang Thinh <jonhnathankevin@yahoo.com.vn>
    Date:   Sun Jul 3 10:51:03 2022 +0700
    
        [UPDATE] update routes
    
    commit a4fe3dc7ad27bfb49658a1e4403a24146f233385
    Author: Hoang Thinh <jonhnathankevin@yahoo.com.vn>
    Date:   Sun Jul 3 10:46:54 2022 +0700
    
        [FIX] merge conflict

--- src/components/animatedRoutes.js
+++ src/components/animatedRoutes.js
@@ -1,8 +1,10 @@
 import Loading from "../pages/Loading/loading";
 import IntroVideoLandingPage from "../pages/IntroVideoLandingPage/introVideoLandingPage";
-import ZoomAboutPage from "../pages/ZoomAboutPage/zoomAboutPage";
-import FocusPage from "../pages/Focus/focus";
 import { Routes, Route, useLocation } from "react-router-dom";
+
+import SignUp from "../pages/SignUp/SignUp";
+import Investor from "../pages/Investor/Investor";
+import FocusPage from "../pages/Focus/focus";
 
 import { AnimatePresence } from "framer-motion";
 import About from "../pages/About/About";
@@ -19,8 +21,10 @@ function AnimatedRoutes() {
           path="/introvideolandingpage"
           element={<IntroVideoLandingPage />}
         />
-        <Route path="zoomaboutpage" element={<ZoomAboutPage />} />
-        <Route path="focus" element={<FocusPage/>}/>
+        <Route path="focus" element={<FocusPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/investor" element={<Investor />} />
+        <Route path="focus" element={<FocusPage />} />
       </Routes>
     </AnimatePresence>
   );
---
+++ src/pages/Investor/Investor.js
@@ -0,0 +1,40 @@
+import "./Investor.css";
+import BackgroundVideo from "./../../assets/video/Section3BG.mov";
+import { useState } from "react";
+import Rock4 from "./../../assets/KKDAO_Rock/Rock04.png";
+import Rock5 from "./../../assets/KKDAO_Rock/Rock05.png";
+
+const Investor = () => {
+  const [isDone, setIsDone] = useState(false);
+
+  return (
+    <div id="investor-root-container">
+      <div id="investor-rocks-layer">
+        <img src={Rock4} alt="Rock4" id="investor-rock4" />
+        <img src={Rock5} alt="Rock5" id="investor-rock5" />
+      </div>
+      <video
+        src={BackgroundVideo}
+        autoPlay
+        muted
+        onEnded={() => setIsDone(true)}
+        id="investor-video"
+      />
+
+      {isDone && (
+        <div id="infoContainer">
+          <h1 id="titleText">MISTLETOE</h1>
+          <p id="subtitleText">
+            Mistletoe is a venture capital firm that prefers to invest in start
+            up companies. The firm seeks to invest in the education, software
+            and support sectors. It is based in Tokyo, Japan and was founded in
+            2013.
+          </p>
+          <p id="largeTitle">THE INVESTOR</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Investor;
