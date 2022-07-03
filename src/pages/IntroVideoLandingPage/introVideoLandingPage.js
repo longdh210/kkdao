954a22e67d6c03f762a99374ee09b6ab5600479a
longdh210 <longdh210@gmail.com>
2022-07-03 09:48:54 +0700

    [UPDATE] fix animation

--- src/pages/IntroVideoLandingPage/introVideoLandingPage.js
+++ src/pages/IntroVideoLandingPage/introVideoLandingPage.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import "./introVideoLandingPage.css";
 import SocialButtons from "../../components/SocialButtons";
 import ZoomVideo from "../../assets/video/1920x1080-Zoom.mp4";
+import { motion } from "framer-motion";
 
 function IntroVideo1() {
     const [isDone, setIsDone] = useState(false);
@@ -38,17 +39,45 @@ function IntroVideo1() {
                 id='video'
             />
             {isDone ? (
-                <div className='content'>
+                <motion.div
+                    className='content'
+                    initial={{ opacity: 0, transition: { duation: 1 }, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                >
                     <SocialButtons leftIcon={<Logo />} />
-                    <h1 className='title'>
+                    <motion.h1
+                        className='title'
+                        initial={{
+                            opacity: 0,
+                            transition: { duation: 1 },
+                            y: 100,
+                        }}
+                        animate={{ opacity: 1, y: 0 }}
+                    >
                         K&nbsp;&nbsp;K&nbsp;&nbsp;D&nbsp;&nbsp;A&nbsp;&nbsp;O
-                    </h1>
-                    <div className='mainContent'>
+                    </motion.h1>
+                    <motion.div
+                        className='mainContent'
+                        initial={{
+                            opacity: 0,
+                            transition: { duation: 1.5 },
+                            y: 100,
+                        }}
+                        animate={{ opacity: 1, y: 0 }}
+                    >
                         <p>
                             FIRST DAO RUN BY A VC FIRM<br></br>IN SOUTHEAST ASIA{" "}
                         </p>
-                    </div>
-                    <div className='wrapButton'>
+                    </motion.div>
+                    <motion.div
+                        className='wrapButton'
+                        initial={{
+                            opacity: 0,
+                            transition: { duation: 2 },
+                            y: 100,
+                        }}
+                        animate={{ opacity: 1, y: 0 }}
+                    >
                         <button
                             id='button'
                             onClick={() => {
@@ -57,8 +86,8 @@ function IntroVideo1() {
                         >
                             STEP IN
                         </button>
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
             ) : (
                 <></>
             )}
